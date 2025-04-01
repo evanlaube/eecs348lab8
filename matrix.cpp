9adee57c38c87f0baeb60d389a9967afe85d6e8b
Evan Laube <laubeevan@gmail.com>
2025-04-01 03:16:08 -0500

    Finish matrix methods to pass tests only

--- matrix.cpp
+++ matrix.cpp
@@ -1,4 +1,6 @@
 #include "matrix.hpp"
+#include <iostream>
+#include <stdexcept>
 
 // implementation of functions declared in matrix.hpp goes here
 // function definitions for a class have their names prefixed with
@@ -8,10 +10,118 @@
 
 Matrix::Matrix(size_t N) {
     // initialize an empty NxN matrix
+    data = std::vector<int>(N*N);
+    size = N;
 }
 
-// e.g. for a member function:
+Matrix::Matrix(std::vector<std::vector<int> > nums) {
+    size = nums.size();
+    data = std::vector<int>(size*size);
+
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            data[i*size+j] = nums[i][j];
+        }
+    }
+}
 
 void Matrix::print_matrix() const {
-    // print out the matrix
+    for(int i = 0; i < size; i++) {
+        for(int n = 0; n < size; n++) {
+            std::cout << data[size*i + n] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
+
+Matrix Matrix::operator+(const Matrix &rhs) const {
+    if(size != rhs.size) {
+        throw std::runtime_error("Attempted to add matrices of different sizes");
+    }
+    Matrix result(size);
+    for(int i = 0; i < size; i++) {
+        for(int n = 0; n < size; n++) {
+            result.data[i*size + n] = data[i*size+n] + rhs.data[i*size+n];
+        }
+    }
+
+    return result;
+}
+
+Matrix Matrix::operator*(const Matrix &rhs) const {
+    if(size != rhs.size) {
+        throw std::runtime_error("Attempted to multiply matrices of different sizes");
+    }
+
+    Matrix result(size);
+
+    for(int row = 0; row < size; row++) {
+        for(int col = 0; col < size; col++) {
+            for(int k = 0; k < size; k++) {
+                result.data[row*size + col] += data[row * size + k] * rhs.data[k * size + col];
+            }
+        }
+    }
+
+    return result;
+}
+
+void Matrix::set_value(std::size_t i, std::size_t j, int n) {
+    if(i >= size || j >= size) {
+        throw std::out_of_range("Attempted to set matrix cell out of bounds");
+    }
+
+    data[i*size+j] = n;
+}
+
+int Matrix::get_value(std::size_t i, std::size_t j) const {
+    if(i >= size || j >= size) {
+        throw std::out_of_range("Attempted to get matrix cell out of bounds");
+    }
+
+    return data[i*size+j];
+}
+
+int Matrix::get_size() const {
+    return size;
+}
+
+
+int Matrix::sum_diagonal_major() const {
+    int sum = 0;
+    std::cout << "Major diagonal: ";
+    for(int i = 0; i < size; i++) {
+        sum += data[i*size + i];
+        std::cout << data[i*size + i] << " "; 
+    }
+    std::cout << std::endl << "Sum: " << sum << std::endl;
+    return sum;
+}
+
+int Matrix::sum_diagonal_minor() const {
+    int sum = 0;
+    std::cout << "Minor diagonal: "; 
+    for(int i = 0; i < size; i++) {
+        sum += data[i*size + (size-1-i)];
+        std::cout << data[i*size + (size-1-i)] << " ";
+    }
+    std::cout << std::endl << "Sum: " << sum << std::endl;
+    return sum;
+}
+
+void Matrix::swap_rows(std::size_t r1, std::size_t r2) {
+    for(int i = 0; i < size; i++) {
+        int temp = data[r1*size + i];
+        data[r1*size+i] = data[r2*size+i];
+        data[r2*size+i] = temp;
+    }
+}
+
+void Matrix::swap_cols(std::size_t c1, std::size_t c2) {
+    for(int i =0; i < size; i++) {
+        int temp = data[i*size + c1];
+        data[i*size+c1] = data[i*size+c2];
+        data[i*size+c2] = temp;
+    }
+}
+
