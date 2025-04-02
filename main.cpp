fd1c367ffe220daaaa7c81102b97f12d1968fd48
Evan Laube <laubeevan@gmail.com>
2025-04-02 14:25:58 -0500

    Comments!

--- main.cpp
+++ main.cpp
@@ -5,31 +5,43 @@
 #include <vector>
 
 int main(int argc, char *argv[]) {
+    // Prompt the user for an input file
     std::cout << "Enter name of file to process: ";
+    // Create a string to store the filename
     std::string filename;
+    // Take user input and store in filename
     std::cin >> filename;
 
+    // Create an input file stream with the input file name
     std::ifstream inputFile(filename);
 
+    // Create new matrices a and b with dimension 0
     Matrix a(0);
     Matrix b(0);
 
+    // Make sure that the file exists
     if(inputFile.is_open()) {
+        // Create a string to store each line
         std::string line;
-
+        // Initialize the size of the matrices to zero
         int size = 0;
+
         try {
+            // Take the first line of the file as the size of the matrices
             std::string sizeString;
             std::getline(inputFile, sizeString);
 
+            // Cast the size string to an integer
             size = stoi(sizeString);
+            // Reconstruct matrices to be proper size
             a = Matrix(size);
             b = Matrix(size);
-        } catch(...) {
+        } catch(...) { // If the first line cannot be cast to an int
+            // Print an error to the user and return
             std::cerr << "Input file in invalid format." << std::endl;
             return 1;
         }
-        // Get next line
+        // Get next line after size
         std::getline(inputFile, line);
 
         // Skip all blank lines that may have been inserted
@@ -42,13 +54,19 @@ int main(int argc, char *argv[]) {
         for(int i = 0; i < size; i++) {
             // Add line data to Matrix
             std::vector<int> data(size);
+            // Convert the line to a stringstream to easily cast to int
             std::stringstream ss(line);
+            // Store each number in 'number'
             int number;
+            // Column number 'n'
             int n = 0;
-            while(ss >> number) {
+            while(ss >> number) { // While there are numbers to read in a row
+                // Set the value at row i and col n to number
                 a.set_value(i, n, number);
+                // Increment the column
                 n++;
             }
+            // Get the next line
             std::getline(inputFile, line);
         }
 
@@ -57,42 +75,54 @@ int main(int argc, char *argv[]) {
         for(int i = 0; i < size; i++) {
             // Add line data to Matrix
             std::vector<int> data(size);
+            // Convert the line to a stringstream to easily cast to int
             std::stringstream ss(line);
+            // Store each number in 'number'
             int number;
+            // Column number 'n'
             int n = 0;
-            while(ss >> number) {
+            while(ss >> number) { // While there are numbers to read in a row
+                // Set the value at row i and col n to number
                 b.set_value(i, n, number);
+                // Increment the column
                 n++;
             }
+            // Get next line
             std::getline(inputFile, line);
         }
 
-        // Print matrices to the user
+        // Print Matrix A to user
         std::cout << "Matrix A:" << std::endl;
         a.print_matrix();
         std::cout << std::endl;
 
+        // Print Matrix B to user
         std::cout << "Matrix B:" << std::endl;
         b.print_matrix();
         std::cout << std::endl;
 
+        // Demo matrix addition
         std::cout << "Result of A + B: " << std::endl;
         (a+b).print_matrix();
         std::cout << std::endl;
 
+        // Demo matrix multiplication
         std::cout << "Result of A * B: " << std::endl;
         (a*b).print_matrix();
         std::cout << std::endl;
 
+        // Demo the major diagonal sums
         std::cout << "Sum of major diagonal elements in A: " << a.sum_diagonal_major() << std::endl;
         std::cout << "Sum of major diagonal elements in B: " << b.sum_diagonal_major() << std::endl;
         std::cout << std::endl;
 
+        // Demo the minor diagonal sums
         std::cout << "Sum of minor diagonal elements in A: " << a.sum_diagonal_minor() << std::endl;
         std::cout << "Sum of minor diagonal elements in B: " << b.sum_diagonal_minor() << std::endl;
         std::cout << std::endl;
 
-        if(size > 2) {
+        if(size > 2) { // If the size of the matrices is more than 2
+            // Demo the row swapping function by swapping row 2 and 3 of matrix A
             Matrix cpy = a;
             cpy.swap_rows(1, 2);
             std::cout << "Result of swapping row 2 of Matrix A with row 3" << std::endl;
@@ -100,7 +130,8 @@ int main(int argc, char *argv[]) {
             std::cout << std::endl;
         }
 
-        if(size > 3) {
+        if(size > 3) { // If the size is more than 3
+            // Demo the column swapping function by swapping column 3 and 4 of matrix B
             Matrix cpy = b;
             cpy.swap_cols(2,3);
             std::cout << "Result of swapping col 3 of Matrix B with col 4" << std::endl;
@@ -109,7 +140,8 @@ int main(int argc, char *argv[]) {
         }
 
 
-    } else {
+    } else { // If the file is unable to be opened
+        // Print an error to the user and return
         std::cerr << "Unable to open file. Make sure it exists and has valid permissions" << std::endl;
         return 1;
     }
--- matrix.cpp
+++ matrix.cpp
@@ -12,13 +12,18 @@
 Matrix::Matrix(size_t N) {
     // initialize an empty NxN matrix
     data = std::vector<int>(N*N);
+    // Set size to N
     size = N;
 }
 
 Matrix::Matrix(std::vector<std::vector<int> > nums) {
+    // Set size to the number of rows/cols
     size = nums.size();
+    // Allocate space for data
     data = std::vector<int>(size*size);
 
+    // Store all data as a 1d array, where elements can be indexed with the formula:
+    //      index = (row*size) + col
     for(int i = 0; i < size; i++) {
         for(int j = 0; j < size; j++) {
             data[i*size+j] = nums[i][j];
@@ -29,12 +34,17 @@ Matrix::Matrix(std::vector<std::vector<int> > nums) {
 void Matrix::print_matrix() const {
     // Get largest number of digits of number in matrix
     int maxDigits = 0;
+    // Loop through each entry in the matrix
     for(int i = 0; i < size*size; i++) {
+        // Get the number at position i
         int number = data[i];
+        // If the number is negative, make it positive
         if(number < 0) {
             number = -number;
         }
+        // Start the number of digits at zero
         int digits = 0;
+        // Calculate the number of digits in the number by repetitively diving by 10
         if(number == 0) {
             digits = 0;
         } else {
@@ -44,11 +54,14 @@ void Matrix::print_matrix() const {
             }
         }
 
+        // Set maxDigits to the max of digits and itself
         maxDigits = std::max(digits, maxDigits);
     }
 
+    // Print each entry in the matrix
     for(int i = 0; i < size; i++) {
         for(int n = 0; n < size; n++) {
+            // Format to print each number the same width
             std::cout << std::setw(maxDigits) << std::setfill('0') << data[size*i + n] << " ";
         }
         std::cout << std::endl;
@@ -56,86 +69,115 @@ void Matrix::print_matrix() const {
 }
 
 Matrix Matrix::operator+(const Matrix &rhs) const {
+    // Make sure that the matrices have the same size
     if(size != rhs.size) {
         throw std::runtime_error("Attempted to add matrices of different sizes");
     }
+    // Create a result matrix of the same size
     Matrix result(size);
     for(int i = 0; i < size; i++) {
         for(int n = 0; n < size; n++) {
+            // For each element in the result, set it equal to that position in a and b
             result.data[i*size + n] = data[i*size+n] + rhs.data[i*size+n];
         }
     }
 
+    // Return the result matrix
     return result;
 }
 
 Matrix Matrix::operator*(const Matrix &rhs) const {
+    // Make sure that the matrices are the same size
     if(size != rhs.size) {
         throw std::runtime_error("Attempted to multiply matrices of different sizes");
     }
 
+    // Create a result matrix of the same size
     Matrix result(size);
 
+    // O(n^3) algorithm to calculate the result of multiplication
     for(int row = 0; row < size; row++) {
         for(int col = 0; col < size; col++) {
             for(int k = 0; k < size; k++) {
+                // For each element, sum the row of the lhs(this) multiplied by the col of the rhs
                 result.data[row*size + col] += data[row * size + k] * rhs.data[k * size + col];
             }
         }
     }
 
+    // Return result matrix
     return result;
 }
 
 void Matrix::set_value(std::size_t i, std::size_t j, int n) {
+    // Make sure that the row and column index are in bounds
     if(i >= size || j >= size) {
         throw std::out_of_range("Attempted to set matrix cell out of bounds");
     }
 
+    // Set the entry at index row*size+col to the input data
     data[i*size+j] = n;
 }
 
 int Matrix::get_value(std::size_t i, std::size_t j) const {
+    // Make sure that the row and column index are in bounds
     if(i >= size || j >= size) {
         throw std::out_of_range("Attempted to get matrix cell out of bounds");
     }
 
+    // Return the entry at index row*size+col
     return data[i*size+j];
 }
 
 int Matrix::get_size() const {
+    // Return the size of the matrix
     return size;
 }
 
 
 int Matrix::sum_diagonal_major() const {
+    // Start sum at zero
     int sum = 0;
+    // Loop through all points where row index = col index
     for(int i = 0; i < size; i++) {
+        // Add the entry at the position to sum
         sum += data[i*size + i];
     }
+    // Return the final sum
     return sum;
 }
 
 int Matrix::sum_diagonal_minor() const {
+    // Start sum at zero
     int sum = 0;
+    // Loop through all points from top right of matrix to bottom left
     for(int i = 0; i < size; i++) {
+        // Add each entry to sum
         sum += data[i*size + (size-1-i)];
     }
     return sum;
 }
 
 void Matrix::swap_rows(std::size_t r1, std::size_t r2) {
+    // Loop size times
     for(int i = 0; i < size; i++) {
+        // Store entry in col i, r1 into temp
         int temp = data[r1*size + i];
+        // Set the entry in r1 to the entry in r2
         data[r1*size+i] = data[r2*size+i];
+        // Set the entry in r2 to temp
         data[r2*size+i] = temp;
     }
 }
 
 void Matrix::swap_cols(std::size_t c1, std::size_t c2) {
+    // Loop size times
     for(int i =0; i < size; i++) {
+        // Store entry in row i, c1 into temp
         int temp = data[i*size + c1];
+        // Set the entry in c1 to the entry in c2
         data[i*size+c1] = data[i*size+c2];
+        // Set the entry in c2 to temp
         data[i*size+c2] = temp;
     }
 }
